afd459bca7f5157211ee1d004cf4c1fff9ddaa84
Durai29 <muruga6129@gmail.com>
2025-06-12 15:56:03 +0530

    Added Activation Function

--- Neuron.py
+++ Neuron.py
@@ -24,7 +24,24 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs,self.weights) + self.biases
 
+class Activation_RELU():
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax():
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis = 1,keepdims=True))
+        self.output = exp_values / np.sum(exp_values,axis=1,keepdims=True)
+
 X, y = spiral_data(samples=100,classes=3)
 dense1 = Layer_Dense(2,3)
-dense1.forward(X[0:5])
-print(dense1.output)
+activation1 = Activation_RELU()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
